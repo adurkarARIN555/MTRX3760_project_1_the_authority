92bf30fb4bbc6c93dadee2cf4605a9f2b6725c9c
eashax <160305643+eashax@users.noreply.github.com>
2024-09-10 22:00:32 +1000

    Added latest code, wall following for first maze

---
+++ turtlebot3_gazebo/src/turtlebot3_drive.cpp
@@ -0,0 +1,141 @@
+#include "turtlebot3_gazebo/turtlebot3_drive.hpp"
+#include <memory>
+#include <chrono>
+
+using namespace std::chrono_literals;
+
+Turtlebot3Drive::Turtlebot3Drive()
+: Node("turtlebot3_drive_node")
+{
+  /************************************************************
+  ** Initialise variables
+  ************************************************************/
+  scan_data_[0] = 0.0;
+  scan_data_[1] = 0.0;
+  scan_data_[2] = 0.0;
+
+  robot_pose_ = 0.0;
+  prev_robot_pose_ = 0.0;
+
+  /************************************************************
+  ** Initialise ROS publishers and subscribers
+  ************************************************************/
+  auto qos = rclcpp::QoS(rclcpp::KeepLast(10));
+
+  // Initialise publishers
+  cmd_vel_pub_ = this->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", qos);
+
+  // Initialise subscribers
+  scan_sub_ = this->create_subscription<sensor_msgs::msg::LaserScan>(
+    "scan", rclcpp::SensorDataQoS(), std::bind(
+      &Turtlebot3Drive::scan_callback, this, std::placeholders::_1));
+  odom_sub_ = this->create_subscription<nav_msgs::msg::Odometry>(
+    "odom", qos, std::bind(&Turtlebot3Drive::odom_callback, this, std::placeholders::_1));
+
+  /************************************************************
+  ** Initialise ROS timers
+  ************************************************************/
+  update_timer_ = this->create_wall_timer(10ms, std::bind(&Turtlebot3Drive::update_callback, this));
+
+  RCLCPP_INFO(this->get_logger(), "Turtlebot3 simulation node has been initialised");
+}
+
+Turtlebot3Drive::~Turtlebot3Drive()
+{
+  RCLCPP_INFO(this->get_logger(), "Turtlebot3 simulation node has been terminated");
+}
+
+/********************************************************************************
+** Callback functions for ROS subscribers
+********************************************************************************/
+void Turtlebot3Drive::odom_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
+{
+  tf2::Quaternion q(
+    msg->pose.pose.orientation.x,
+    msg->pose.pose.orientation.y,
+    msg->pose.pose.orientation.z,
+    msg->pose.pose.orientation.w);
+  tf2::Matrix3x3 m(q);
+  double roll, pitch, yaw;
+  m.getRPY(roll, pitch, yaw);
+
+  robot_pose_ = yaw;
+}
+
+void Turtlebot3Drive::scan_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
+{
+  uint16_t scan_angle[3] = {0, 30, 330};
+
+  for (int num = 0; num < 3; num++) {
+    if (std::isinf(msg->ranges.at(scan_angle[num]))) {
+      scan_data_[num] = msg->range_max;
+    } else {
+      scan_data_[num] = msg->ranges.at(scan_angle[num]);
+    }
+  }
+}
+
+void Turtlebot3Drive::update_cmd_vel(double linear, double angular)
+{
+  geometry_msgs::msg::Twist cmd_vel;
+  cmd_vel.linear.x = linear;
+  cmd_vel.angular.z = angular;
+
+  cmd_vel_pub_->publish(cmd_vel);
+}
+
+/********************************************************************************
+** Update functions
+********************************************************************************/
+void Turtlebot3Drive::update_callback()
+{
+  // Constants for distance thresholds and speeds
+  const double DIST_THRESHOLD_FRONT = 0.5; // Distance threshold for front wall
+  const double DIST_THRESHOLD_LEFT = 0.5;  // Distance threshold for left wall
+  const double DIST_THRESHOLD_RIGHT = 0.5; // Distance threshold for right wall
+  const double LINEAR_SPEED = 0.2;         // Linear speed
+  const double TURN_SPEED = 1.0;           // Turning speed for sharper turns
+  const double TURN_SPEED_SLOW = 0.4;      // Slower turning speed for wider turns
+  //const double TURN_ANGLE = 0.2;           // Angle adjustment to handle turning
+
+  // Define sensor data for convenience
+  double front_distance = scan_data_[0];
+  double left_distance = scan_data_[1];
+  double right_distance = scan_data_[2];
+
+  // Initialize linear and angular speeds
+  double linear_speed = LINEAR_SPEED;
+  double angular_speed = 0.0;
+
+  // Decision making based on sensor data
+  if (left_distance < DIST_THRESHOLD_LEFT) {
+    // Wall on the left is too close, turn right sharply
+    angular_speed = -TURN_SPEED;
+  } else if (front_distance < DIST_THRESHOLD_FRONT) {
+    // Wall in front is too close, turn left sharply
+    angular_speed = TURN_SPEED;
+  } else if (right_distance < DIST_THRESHOLD_RIGHT) {
+    // Wall on the right, path is clear, move forward with slight left turn
+    linear_speed = LINEAR_SPEED;
+    angular_speed = TURN_SPEED_SLOW; // Slight turn left to follow the wall
+  } else {
+    // No significant obstacles detected, maintain forward movement with slight left turn
+    linear_speed = LINEAR_SPEED;
+    angular_speed = TURN_SPEED_SLOW;
+  }
+
+  // Update robot velocity
+  update_cmd_vel(linear_speed, angular_speed);
+}
+
+/*******************************************************************************
+** Main
+*******************************************************************************/
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<Turtlebot3Drive>());
+  rclcpp::shutdown();
+
+  return 0;
+}
